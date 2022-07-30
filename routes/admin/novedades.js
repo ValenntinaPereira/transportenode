8a4a35e00cb02826efbc64390a14e1c93c7afd66
Valentina Pereira <valentinapereira22@hotmail.com>
2022-07-30 13:40:19 -0300

    funcionalidad del agregar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -7,17 +7,42 @@ router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
 
-  res.render('admin/novedades',{
+  res.render('admin/novedades', {
     layout: 'admin/layout',
-    persona:req.session.nombre,
+    persona: req.session.nombre,
     novedades
   });
 });
 // esto sirve para mostrar el alta las novedades 
-router.get('/agregar',(req,res,next) =>{
-  res.render('admin/agregar',{
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
     layout: 'admin/layout'
   })
+})
+router.post('/agregar', async (req, res, next) => {
+
+  console.log(req.body)
+  try {
+    if (req.body.titulo != '' && req.body.subtitulo != '' && req.body.cuerpo != '') {
+      await novedadesModel.insertNovedades(req.body)
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout : 'admin/layout',
+        error: true,
+        message : 'Todos los campos son obligatorios'
+      })
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.render('admin/agregar',{
+      layout: 'admin/layout',
+      error:true,
+      message: 'La novedad no se guardo correctamente'
+    })
+  }
+
 })
 
 module.exports = router;
