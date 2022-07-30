30d186d385fa83c87e6ea5b698b8d8a1d69deaca
Valentina Pereira <valentinapereira22@hotmail.com>
2022-07-30 00:31:59 -0300

    terminado de novedades,diseño de agregar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('admin/novedades',{
     layout: 'admin/layout',
-    persona:req.session.nombre 
+    persona:req.session.nombre,
+    novedades
   });
 });
+// esto sirve para mostrar el alta las novedades 
+router.get('/agregar',(req,res,next) =>{
+  res.render('admin/agregar',{
+    layout: 'admin/layout'
+  })
+})
 
 module.exports = router;
